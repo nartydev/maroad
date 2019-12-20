ee6352cbf354714cc142db4b0ee0f5b8dec3dd52
Kérian Pelat <narty.dev@gmail.com>
2019-12-20 03:53:42 +0100

    Map

--- pages/agenda.js
+++ pages/agenda.js
@@ -1,8 +1,8 @@
 import PageLayout from '../layouts/page.js';
-
-import EventList from '../components/EventList';
+import dynamic from 'next/dynamic';
 import Button from '../components/Button';
 import MaxWidth from '../components/MaxWidth';
+const EventList = dynamic(() => import('../components/EventList/index'), { ssr: false });
 
 const Agenda = () => {
 	return (
